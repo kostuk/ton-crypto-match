6d38c62ace6bce53a045059e1bdab4d2637957b8
NIkolas <nikolas@ukr.net>
2024-05-11 16:06:26 +0300

    Add copy

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import '@telegram-apps/telegram-ui/dist/styles.css';
+import './index.css';
+
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+
+const root = createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(<App />);
